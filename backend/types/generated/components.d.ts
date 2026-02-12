3834a7712f0ecdc918a395bc5e2d838b17f9a312
carmen637 <carmen.ggar96@gmail.com>
2026-02-12 21:19:58 +0100

    Content type Libro con campos

---
+++ backend/types/generated/components.d.ts
@@ -0,0 +1,28 @@
+import type { Schema, Struct } from '@strapi/strapi';
+
+export interface SharedSeo extends Struct.ComponentSchema {
+  collectionName: 'components_shared_seos';
+  info: {
+    displayName: 'seo';
+    icon: 'globe';
+  };
+  attributes: {
+    keywords: Schema.Attribute.String;
+    metaDescription: Schema.Attribute.String &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 160;
+      }>;
+    metaTitle: Schema.Attribute.String &
+      Schema.Attribute.SetMinMaxLength<{
+        maxLength: 60;
+      }>;
+  };
+}
+
+declare module '@strapi/strapi' {
+  export module Public {
+    export interface ComponentSchemas {
+      'shared.seo': SharedSeo;
+    }
+  }
+}
