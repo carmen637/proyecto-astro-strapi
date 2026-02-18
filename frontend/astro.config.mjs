13e96bacc9964cf3731abc39096d0c3955c2e47c
carmen637 <carmen.ggar96@gmail.com>
2026-02-18 18:18:58 +0100

    Cambiar a output static

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,5 +2,6 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  output: 'static', // Añade esta línea
   integrations: [tailwind()]
 });
