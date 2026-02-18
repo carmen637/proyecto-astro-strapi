01f54297340b4fb9d02a46b8f8229dd96f9d9e22
carmen637 <carmen.ggar96@gmail.com>
2026-02-18 19:42:39 +0100

    Astro desplegado en AWS con fetch client-side

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -2,6 +2,9 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
-  output: 'static', // Añade esta línea
-  integrations: [tailwind()]
+  output: 'static',
+  integrations: [tailwind()],
+  build: {
+    inlineStylesheets: 'auto'
+  }
 });
