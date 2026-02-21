e62aa55113248da5ef668c3178227e4302107e4d
carmen637 <carmen.ggar96@gmail.com>
2026-02-21 02:22:27 +0100

    Anadido el config adapter node

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,8 +1,10 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
+import node from '@astrojs/node';
 
 export default defineConfig({
   output: 'static',
+  adapter: node({ mode: 'standalone' }),
   integrations: [tailwind()],
   build: {
     inlineStylesheets: 'auto'
