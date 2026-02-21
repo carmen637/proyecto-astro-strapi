12d753f532bbd6df491090a181740a432f1772d0
carmen637 <carmen.ggar96@gmail.com>
2026-02-21 02:04:03 +0100

    Simulador tirada, menú hamburguesa, login, registro, cuenta

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,10 +1,17 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
+import node from '@astrojs/node';
+
 export default defineConfig({
   output: 'static',
   integrations: [tailwind()],
+
   build: {
     inlineStylesheets: 'auto'
-  }
+  },
+
+  adapter: node({
+    mode: 'standalone'
+  })
 });
