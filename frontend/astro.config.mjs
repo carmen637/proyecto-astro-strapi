7bc31f12117796c073883eb2cfb9d9741f0e119b
carmen637 <carmen.ggar96@gmail.com>
2026-02-22 13:19:33 +0100

    Añadir buscador, filtros, paginacion, sitemap y robots

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,11 +1,15 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 import node from '@astrojs/node';
+import sitemap from '@astrojs/sitemap';
+
+import sitemap from '@astrojs/sitemap';
 
 export default defineConfig({
+  site: 'http://arcanalibros.com',
   output: 'static',
   adapter: node({ mode: 'standalone' }),
-  integrations: [tailwind()],
+  integrations: [tailwind(), sitemap()],
   build: {
     inlineStylesheets: 'auto'
   }
