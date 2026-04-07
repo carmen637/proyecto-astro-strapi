53c3d7b7f35dd615a2ae46e75337617d9de47b4a
carmen637 <carmen.ggar96@gmail.com>
2026-04-07 21:48:06 +0200

    update ip

--- frontend/astro.config.mjs
+++ frontend/astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
-import node from '@astrojs/node';
+//import node from '@astrojs/node';
 import sitemap from '@astrojs/sitemap';
 
 
 export default defineConfig({
-  site: 'http://arcanalibros.com',
+  site: 'https://carmen637.github.io',
+  //base: '/proyecto-astro-strapi',
   output: 'static',
-  adapter: node({ mode: 'standalone' }),
+  //adapter: node({ mode: 'standalone' }),
   integrations: [tailwind(), sitemap()],
   build: {
     inlineStylesheets: 'auto'
